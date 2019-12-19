a93b3b15335b395857d63e99e8090e305e80790f
Andrea <areds13@gmail.com>
2019-12-19 21:57:25 +0100

    Candidate entity ranking

--- EntityLinkingClass.py
+++ EntityLinkingClass.py
@@ -2,17 +2,24 @@
 Entity Linking class
 """
 
-import csv, codecs, difflib, Levenshtein, distance
-from ElasticSearchClass import ElasticSearch 
+import csv, codecs, difflib, distance
+from pyjarowinkler import distance as jaro
+from ElasticSearchClass import ElasticSearch
+import statistics
 
 class Entity():
-    def __init__(self, doc_id: str, surface_form: str, spacy_type: str, context:str):
+    def __init__(self, doc_id: str, surface_form: str, spacy_type: str, context = ""):
         self.surface_form = surface_form
         self.doc_id = doc_id
         self.spacy_type = spacy_type
         self.context = context
-        self.linked_entity = dict()
+
+        self.linked_entity = ""
         self.candidates_entities = dict()
+
+        self.elasticsearch_score = 0            # to tune
+        self.linked_entity_similarity = dict()  # to tune
+        self.similarity_score = 0               # to tune
 
 class EntityLinking(ElasticSearch):
     def __init__(self):    
@@ -21,28 +28,37 @@ class EntityLinking(ElasticSearch):
         self.unmatched_entities = []
         self.single_match_candidate_entities = []
         self.multiple_match_candidate_entities = []
+
+        self.elastic_search_threshold = 4.0
+        self.similarity_threshold = 0.8
         
     def import_entities(self):
         entities = list()
         # Load file with all recognized entity surface forms and the IDs of the documents they were found on.
         with open("data/sample-output.tsv") as tsv:
             for line in csv.reader(tsv, dialect="excel-tab"):
-                entity = Entity(line[0], line[1],  line[2], line[3])
-                entities.append(entity)
+                if len(line) == 4:
+                    entity = Entity(line[0], line[1],  line[2], line[3])
+                    entities.append(entity)
+                elif len(line) == 3:
+                    entity = Entity(line[0], line[1],  line[2])
+                    entities.append(entity)
+                else:
+                    print("FOUND LINE OF SIZE " + str(len(line)))
         
         return entities
 
-    def similar(self, string1, string2, method = 'Sorensen') -> float:
+    def similar(self, string1, string2, method = 'Jaro') -> float:
         score = 0
         
         if method == 'Exact Matching':
             score = 1 if string1 == string2 else 0
         elif method ==  'Sequence Matching':
             score = difflib.SequenceMatcher(None, string1, string2).ratio()
-        elif method == 'Levenshtein':
-            score = Levenshtein.ratio(string1, string2)
         elif method == 'Sorensen':
             score = 1 - distance.jaccard(string1, string2)
+        elif method == 'Jaro':
+            score = jaro.get_jaro_distance(string1, string2, winkler=True, scaling=0.1)
         
         return score 
     
@@ -71,7 +87,7 @@ class EntityLinking(ElasticSearch):
     def get_elasticsearch_candidate_entities(self):
         entities = self.import_entities()
         
-        for entity in entities[0:1000]:
+        for entity in entities[0:100]:
             #print(token[2])
             
             elastic_search_result = self.search_elasticsearch(entity.surface_form)
@@ -119,12 +135,64 @@ class EntityLinking(ElasticSearch):
                 if avg( float_values ) > max:
                     max = avg( float_values )
                     max_key = key
+
+                entity.elasticsearch_score = max# to tune
             return entity.candidates_entities[max_key]
 
         for entity in entities:
             entity.linked_entity = get_candidate_entity_with_higher_avg_score(entity)
 
-    def discriminate_on_elasticsearch_score_and_similarity(self, entities: list):
+    def discriminate(self, entities: list):
+        def avg(my_list :list ) -> float: 
+            return sum(my_list) / len(my_list)
+
+        def avg_similarity(my_list: list, method: str) -> float:
+            sum = 0
+            for string in my_list:
+                sum = sum + self.similar(entity.surface_form, string, method)
+            avg_similarity = sum/ len(my_list)
+            return avg_similarity
+
+        #returns a dictionary with fb_id and elastic search avg score and
+        # a dctionary with only the fb_id and similarity score of the best candidate
+        def get_candidates_score( entity) -> dict:
+            keys = list( entity.candidates_entities.keys() )
+            max = 0
+            max_key = keys[0]
+
+            max_similarity = 0
+
+            candidates_elastic_scores = {}
+            best_candidate_by_similariry = ()
+
+            #loop on all the freebase id returned by elasticsearch
+            for key in keys:
+                values = list ( entity.candidates_entities[key])
+                # filter only the numbers in elastic search return stuff
+                string_values = [ x for x in values if type(x) is str ]
+                float_values = [ x for x in values if type(x) is not str ]
+
+                avg_elastic_score = avg(float_values)
+                candidates_elastic_scores[key] = avg_elastic_score
+
+                avg_similarity_score = avg_similarity(string_values, 'Jaro')
+                if(avg_similarity_score > max_similarity):
+                    max_similarity = avg_similarity_score
+                    best_candidate_by_similariry = (key, max_similarity)
+
+            return candidates_elastic_scores, best_candidate_by_similariry
+
+        for entity in entities:
+            candidates_elastic_scores, best_candidate_by_similariry = get_candidates_score(entity)
+
+            best_elastic_cand = max(candidates_elastic_scores, key=candidates_elastic_scores.get)
+
+            if (candidates_elastic_scores[best_elastic_cand] >= self.elastic_search_threshold):
+                entity.linked_entity = best_elastic_cand
+            elif best_candidate_by_similariry[1] >= self.similarity_threshold:
+                entity.linked_entity = best_candidate_by_similariry[0]        
+
+    def discriminate_on_elasticsearch_score_and_similarity(self, entities: list, method = "Jaro"):
         def avg(my_list :list ) -> float: 
             return sum(my_list) / len(my_list)
 
@@ -146,18 +214,17 @@ class EntityLinking(ElasticSearch):
                 string_values = [ x for x in values if type(x) is str ]
                 sum = 0
                 for string in string_values:
-                    sum = sum + self.similar(entity.surface_form, string)
+                    sum = sum + self.similar(entity.surface_form, string, method)
                 avg_similarity = sum/ len(string_values)
                 if(avg_similarity > max_similarity):
                     max_similarity = avg_similarity
                     max_key = key
-
+                
+                entity.similarity_score = max_similarity # to tune
             return entity.candidates_entities[max_key]
 
         for entity in entities:
-            entity.linked_entity = get_candidate_entity_with_higher_avg_score(entity)
-            print("ENTITY NAME: " + entity.surface_form + "ENTITY CHOSEN" + str(entity.linked_entity) +"\n")
-                
+            entity.linked_entity_similarity = get_candidate_entity_with_higher_avg_score(entity)     # to tune          
 
 if __name__ == "__main__" :
     print("--- TESTIN entity linking")
@@ -194,7 +261,43 @@ if __name__ == "__main__" :
             print("\n")
     """
 
-    entity_linking.discriminate_on_elasticsearch_score_and_similarity(entity_linking.multiple_match_candidate_entities)
+    #entity_linking.discriminate_on_elasticsearch_score(entity_linking.multiple_match_candidate_entities)
+
+    #entity_linking.discriminate_on_elasticsearch_score_and_similarity(    entity_linking.multiple_match_candidate_entities)
+
+    entity_linking.discriminate(entity_linking.multiple_match_candidate_entities)
+
+    sum_similarity_score = 0
+    sum_elastic_score = 0
+    max_similarity = 0
+    max_elastic_score = 0
+    elastic_score_list =[]
+    similarity_score_list = []
+
+    
+    for entity in entity_linking.multiple_match_candidate_entities:
+        """
+        print("NAME: " + entity.surface_form + " TYPE "  + entity.spacy_type + " SC SCORE " + str(entity.elasticsearch_score) + " SIM SCORE " + str(entity.similarity_score) + " EL LINK " + str(entity.linked_entity) + " SM LINK " + str(entity.linked_entity_similarity) + " CONTEXT " + str(entity.context) + "\n CANDIDATES ENTITY: \n" + str(entity.candidates_entities) + "\n")
+        """
+        similarity_score_list.append(entity.similarity_score)
+        elastic_score_list.append(entity.elasticsearch_score)
+        
+
+    avg_similarity_score = statistics.mean(similarity_score_list)
+    avg_elastic_score = statistics.mean(elastic_score_list)
+
+    median_similarity = statistics.median(similarity_score_list)
+    median_elastic =  statistics.median(elastic_score_list)
+
+    std_similarity = statistics.stdev(similarity_score_list)
+    std_elastic = statistics.stdev(elastic_score_list)
+
+    print("AVG similarity: " + str(avg_similarity_score))
+    print("MEDIAN similarity: " + str(median_similarity))
+    print("STD similarity: " + str(std_similarity))
+    print("AVG elasticsearch " + str(avg_elastic_score))
+    print("MEDIAN elasticsearch " + str(median_elastic))
+    print("STD elastic: " + str(std_elastic))
 
     #entity_linking.file_write_entities(s, "test/multi-output.tsv")
 
