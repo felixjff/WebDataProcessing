b1a27b5e1a2501c03e2a80fdabeab817cc7fab69
Alex <almar2019am@gmail.com>
2019-12-20 23:08:05 +0100

    Waiting for job to end

--- run.sh
+++ run.sh
@@ -10,3 +10,15 @@ echo "----- SETUP: load prun"
 module load prun
 
 prun -o wdps_group13 -v -np 1 -t 600000 runner.sh $1
+
+sleep 10
+
+while : ; do
+    if ls runner.sh.* 1> /dev/null 2>&1; then
+        sleep 5
+    else
+        break
+    fi
+done
+
+cat final-output.tsv
